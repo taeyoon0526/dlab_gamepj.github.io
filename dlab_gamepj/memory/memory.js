4b6ac568ed7388526c365b5d8817ba92c37ba10e
@taeyoon_0526 <iamtaeyoon@icloud.com>
2025-02-19 19:29:23 +0900

    딜레이 감소

--- dlab_gamepj/memory/memory.js
+++ dlab_gamepj/memory/memory.js
@@ -137,7 +137,7 @@ function showCompletionScreen() {
   // 홈 버튼 생성
   const homeBtn = document.createElement("a");
   homeBtn.classList.add("home-btn");
-  homeBtn.href = "#"; // 홈 링크
+  homeBtn.href = "../index.html";
   homeBtn.innerText = "홈으로";
 
   // 버튼 추가
